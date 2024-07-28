b425792482ba96ce87236cb2dd48a85b43e0d5c6
Cassandra Tam <cassandra.wx.tam@gmail.com>
2024-07-28 15:18:49 +1000

    feat(RadioGroup): make generic

--- src/frontend-challenge/components/RadioGroup/RadioGroup.tsx
+++ src/frontend-challenge/components/RadioGroup/RadioGroup.tsx
@@ -1,23 +1,34 @@
 import './RadioGroup.css';
 
-export type RadioGroupProps = {
+export type RadioGroupProps<OptionValues extends string> = {
   legend: string;
   name: string;
-  options: { label: string; value: string }[];
+  activeOption: string;
+  onOptionChange: (value: OptionValues) => void;
+  options: { label: string; value: OptionValues }[];
 };
 
-export const RadioGroup = ({
+export const RadioGroup = <OptionValues extends string>({
   legend,
   name,
+  activeOption,
+  onOptionChange,
   options,
-}: RadioGroupProps): JSX.Element => (
+}: RadioGroupProps<OptionValues>): JSX.Element => (
   <fieldset>
     <legend>{legend}</legend>
     {options.map(({ label, value }) => {
       const inputId = `${name}--${value}`;
       return (
         <div key={value} className="radio-group__options">
-          <input type="radio" name={name} id={inputId} value={value} />
+          <input
+            type="radio"
+            name={name}
+            id={inputId}
+            value={value}
+            checked={value === activeOption}
+            onChange={() => onOptionChange(value)}
+          />
           <label htmlFor={inputId}>{label}</label>
         </div>
       );
