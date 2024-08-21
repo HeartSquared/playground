01b1be614e2c4436a2855082be9d1fd37c980130
Cassandra Tam <cassandra.wx.tam@gmail.com>
2024-08-21 18:53:05 +1000

    test(calculateAnnuallyInterest): fix tests

--- scripts/calculateAnnuallyInterest.spec.ts
+++ scripts/calculateAnnuallyInterest.spec.ts
@@ -2,23 +2,23 @@ import { describe, expect, test } from 'vitest';
 import { calculateAnnuallyInterest } from './calculateAnnuallyInterest';
 
 describe('calculateAnnuallyInterest()', () => {
-  test('calculates interest for 1 quarter', () => {
+  test('calculates interest for 1 year', () => {
     expect(
       calculateAnnuallyInterest({
         startAmount: 10000,
         interestRateDecimal: 0.011,
         paymentsRemaining: 1,
       }),
-    ).toBe(27.5);
+    ).toBe(110);
   });
 
-  test('calculates interest for 2 quarters', () => {
+  test('calculates interest for 2 years', () => {
     expect(
       calculateAnnuallyInterest({
         startAmount: 10000,
         interestRateDecimal: 0.011,
         paymentsRemaining: 2,
       }),
-    ).toBe(55.08);
+    ).toBe(221.21);
   });
 });
