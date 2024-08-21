ba47d822cf8b6d7e1ac9f370aefabed80b5140ee
Cassandra Tam <cassandra.wx.tam@gmail.com>
2024-08-21 20:15:12 +1000

    refactor(convertInterestRateToDecimal): fix result to 4 decimal places

--- scripts/convertInterestRateToDecimal.spec.ts
+++ scripts/convertInterestRateToDecimal.spec.ts
@@ -2,13 +2,19 @@ import { describe, expect, test } from 'vitest';
 import { convertInterestRateToDecimal } from './convertInterestRateToDecimal.ts';
 
 describe('convertInterestRateToDecimal()', () => {
-  test('converts percentage value to float up to 4 decimals', () => {
+  test('converts whole numbers', () => {
     expect(convertInterestRateToDecimal(100)).toBe(1);
     expect(convertInterestRateToDecimal(10)).toBe(0.1);
     expect(convertInterestRateToDecimal(1)).toBe(0.01);
+  });
+
+  test('converts numbers with 1 decimal place', () => {
     expect(convertInterestRateToDecimal(1.1)).toBe(0.011);
-    expect(convertInterestRateToDecimal(123.45)).toBe(1.2345);
     expect(convertInterestRateToDecimal(12.3)).toBe(0.123);
+  });
+
+  test('converts numbers with 2 decimal places', () => {
+    expect(convertInterestRateToDecimal(123.45)).toBe(1.2345);
     expect(convertInterestRateToDecimal(1.23)).toBe(0.0123);
     expect(convertInterestRateToDecimal(0.12)).toBe(0.0012);
     expect(convertInterestRateToDecimal(0.01)).toBe(0.0001);
--- scripts/convertInterestRateToDecimal.ts
+++ scripts/convertInterestRateToDecimal.ts
@@ -1,5 +1,2 @@
-export const convertInterestRateToDecimal = (percentage: number): number => {
-  // Round decimals to 4 decimal places
-  const DECIMAL_OFFSET = 1000;
-  return Math.round(percentage * DECIMAL_OFFSET) / (100 * DECIMAL_OFFSET);
-};
+export const convertInterestRateToDecimal = (percentage: number): number =>
+  Number((percentage / 100).toFixed(4));
